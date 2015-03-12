4352e43b918419bae0b1a083bb83f989fe67ae3f
Vurbana <vurbanoff@gmail.com>
2015-03-12 21:35:02 +0200

    BST implementation without delete function

--- src/structures/BST.java
+++ src/structures/BST.java
@@ -1,50 +1,106 @@
 package structures;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
 /**
  * Created by vankata on 12.03.15.
  */
 public class BST<Key extends Comparable<Key>, Value> {
-    private Node root;
+
+    private Node root = null;
+
     private class Node{
         private Key key;
-        private int N;
         private Value value;
         private Node left, right;
-        public Node(Key key, Value value, int N){
+        public Node(Key key, Value value){
             this.key = key;
             this.value = value;
-            this.N = N;
+            this.left = null;
+            this.right = null;
+
         }
     }
-    public int size(){
-        return size(root);
-    }
-    private int size(Node x){
-        if(x == null){
-            return 0;
-        }else{
-            return x.N;
-        }
-    }
+
     public void put(Key key, Value value){
+        Node parent = null;
+        Node current = root;
+        int cmp;
+        while(current != null){
+            parent = current;
+            cmp = key.compareTo(current.key);
+            if( cmp == 0){
+                break;
+            }else if(cmp < 0){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        if(parent == null){
+            root = new Node(key,value);
+        }else if(key.compareTo(parent.key) == 0){
+            parent.value = value;
+        }else if(key.compareTo(parent.key) < 0){
+            parent.left = new Node(key, value);
+        }else{
+            parent.right = new Node(key,value);
+        }
+    }
+
+    public Value get(Key key){
+        Node current  = root;
+        int cmp;
+        while(current != null){
+            cmp = key.compareTo(current.key);
+            if(cmp == 0){
+                return current.value;
+            }else if(cmp < 0){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return null;
+    }
+
+    public boolean contains(Key key){
+
+        return get(key) != null;
 
     }
-    public Value get(Key key){
-        return get(key, root);
+    public void print(){
+        print(root);
     }
-    private Value get(Key key, Node node){
-        if(node == null){
-            return null;
+    private void print(Node node){
+        if(node != null){
+            print(node.left);
+            System.out.print(String.format("%s %s\n", node.key.toString(), node.value.toString()));
+            print(node.right);
         }
-        if(node.key.compareTo(key)<0){
-            get(key, node.right);
-        }else if (node.key.compareTo(key)>0){
-            get(key, node.left);
+    }
+
+    public Key min(){
+      return  min(root).key;
+    }
+    private Node min(Node node){
+        while(node.left != null){
+            node = node.left;
         }
-        return node.value;
+        return node;
     }
-    public boolean contains(Key key){
-        return get(key) != null;
+    public Key max(){
+        return max(root).key;
+
     }
+    private Node max(Node node){
+        while(node.right != null){
+            node = node.right;
+        }
+        return node;
+    }
+
 
 }
