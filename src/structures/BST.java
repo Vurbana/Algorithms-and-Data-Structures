318e6fc180ca6f725f669f28405eee55c9094a7d
Vurbana <vurbanoff@gmail.com>
2015-03-13 12:45:11 +0200

    Added delete function

--- src/structures/BST.java
+++ src/structures/BST.java
@@ -49,12 +49,20 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
     public Value get(Key key){
+        Node node = getNode(key);
+        if(node != null){
+            return node.value;
+        }else{
+            return null;
+        }
+    }
+    private Node getNode(Key key){
         Node current  = root;
         int cmp;
         while(current != null){
             cmp = key.compareTo(current.key);
             if(cmp == 0){
-                return current.value;
+                return current;
             }else if(cmp < 0){
                 current = current.left;
             }else{
@@ -66,7 +74,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     public boolean contains(Key key){
 
-        return get(key) != null;
+        return getNode(key) != null;
 
     }
     public void print(){
@@ -75,7 +83,7 @@ public class BST<Key extends Comparable<Key>, Value> {
     private void print(Node node){
         if(node != null){
             print(node.left);
-            System.out.print(String.format("%s %s\n", node.key.toString(), node.value.toString()));
+            System.out.print(String.format("<%s,%s>\n", node.key.toString(), node.value.toString()));
             print(node.right);
         }
     }
@@ -99,6 +107,91 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
         return node;
     }
+    private Node getParent(Key key){
+        Node parent = null;
+        Node current = root;
+        int cmp = key.compareTo(current.key);
+        if(cmp == 0){
+            return null;
+        }
+        while(current != null){
+            cmp = key.compareTo(current.key);
+            if( cmp == 0){
+                break;
+            }else if(cmp < 0){
+                parent = current;
+                current = current.left;
+            }else{
+                parent = current;
+                current = current.right;
+            }
+        }
+        return parent;
+    }
+    public void delete(Key key){
+        if(root == null){
+            return;
+        }
+        Node current = getNode(key);
+        if(current != null){
+            Node parent = getParent(key);
+            delete(current, parent);
+        }
+    }
+    private void delete( Node node, Node parent){
+        if(node.left == null && node.right == null){
+            if(parent == null){
+                node = null;
+                root = null;
+            }else{
+                if(parent.left == node){
+                    node = null;
+                    parent.left = null;
+                }else{
+                    node = null;
+                    parent.right = null;
+                }
+            }
 
+        }else if(node.left != null && node.right != null){
+            Node tmp = min(node.right);
+            Node p = getParent(tmp.key);
+            node.key = tmp.key;
+            node.value = tmp.value;
+            delete(tmp, p);
 
-}
+        }else{
+            if(node.left == null){
+                if(parent == null){
+                    root = node.right;
+                    node = null;
+                }else{
+                    if(parent.left == node){
+                        parent.left = node.right;
+                        node = null;
+                    }
+                    else{
+                        parent.right = node.right;
+                        node = null;
+                    }
+                }
+            }else{
+                if(parent == null){
+                    root = node.left;
+                    node = null;
+                }else{
+                    if(parent.left == node){
+                        parent.left = node.left;
+                        node = null;
+                    }else{
+                        parent.right = node.left;
+                        node = null;
+                    }
+                }
+            }
+        }
+
+    }
+
+    }
+
