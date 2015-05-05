97ce571408b0b8cb06a5559631670f0fa75df45a
Vurbana <vurbanoff@gmail.com>
2015-05-06 02:06:03 +0300

    Fixed performance and changed variables names for better understanding

--- Java Algorithms and Data Structures/interval/tree/IntervalTree.java
+++ Java Algorithms and Data Structures/interval/tree/IntervalTree.java
@@ -2,11 +2,10 @@ package interval.tree;
 
 /**
  * Interval Tree implementation in Java
+ *
  * @author: Ivan Vurbanov
  * @keywords: Data Structures, Dynamic RMQ, Interval update and query
- * @modified:
- * 
- * Implement a dynamic RMQ with interval query and update. The update should
+ * @modified: Implement a dynamic RMQ with interval query and update. The update should
  * add a certain value to all cells in a given interval, while the query should
  * return the maximum value inside an interval.
  */
@@ -15,120 +14,137 @@ public class IntervalTree {
     private int[] tree;
     private int n;
     private int[] lazy;
+
     /**
      * Creates a new interval tree with initial values given in values.
      * Each update and query should allow indices [0, values.size() - 1].
      */
     IntervalTree(int[] values) {
+
         setTree(values);
-    }
-    public int pow2roundup(int x) {
-        if (x < 0)
-            return 0;
-        --x;
-        x |= x >> 1;
-        x |= x >> 2;
-        x |= x >> 4;
-        x |= x >> 8;
-        x |= x >> 16;
-        return x + 1;
     }
 
     public void setTree(int[] values) {
         n = values.length;
-        int x = (int)(Math.ceil(Math.log(n)/Math.log(2)))+1;
-        int size = (1 << x);
+        int height = (int) (Math.ceil(Math.log(n) / Math.log(2))) + 1;
+        int size = (1 << height);
         lazy = new int[size];
         this.tree = new int[size];
-        buildTree(1,0,n-1,values);
-
-
+        buildTree(1, 0, n - 1, values);
     }
 
-    void buildTree(int node, int a, int b, int arr[]) {
-        if(a > b) return; // Out of range
-
-        if(a == b) { // Leaf node
-            tree[node] = arr[a]; // Init value
+    void buildTree(int node, int left, int right, int values[]) {
+        if (left > right) {
             return;
         }
 
-        buildTree(node*2, a, (a+b)/2,arr); // Init left child
-        buildTree(node*2+1, 1+(a+b)/2, b,arr); // Init right child
+        if (left == right) {
+            tree[node] = values[left];
+            return;
+        }
+        int middle = getMiddle(left, right);
+        int leftChild = 2 * node;
+        int rightChild = leftChild + 1;
+        buildTree(leftChild, left, middle, values);
+        buildTree(rightChild, middle + 1, right, values);
 
-        tree[node] = Math.max(tree[node*2], tree[node*2+1]); // Init root value
+        tree[node] = Math.max(tree[leftChild], tree[rightChild]);
     }
+
+    private int getMiddle(int left, int right) {
+        return left + ((right - left) / 2);
+    }
+
     /**
      * Adds the value add to each element in the interval [idx1, idx2].
      */
     void update(int idx1, int idx2, int add) {
-        updateTree(1,0,n-1,idx1,idx2, add);
+        updateTree(1, 0, n - 1, idx1, idx2, add);
     }
-    private void updateTree(int node, int a, int b, int i, int j, int value) {
 
+    private void updateTree(int node, int left, int right, int i, int j, int value) {
 
-        if(lazy[node] != 0) { // This node needs to be updated
-            tree[node] += lazy[node]; // Update it
+        int leftChild = 2 * node;
+        int rightChild = leftChild + 1;
+        if (lazy[node] != 0) {
+            tree[node] += lazy[node];
 
-            if(a != b) {
-                lazy[node*2] += lazy[node]; // Mark child as lazy
-                lazy[node*2+1] += lazy[node]; // Mark child as lazy
+            if (left != right) {
+                lazy[leftChild] += lazy[node];
+                lazy[rightChild] += lazy[node];
             }
 
-            lazy[node] = 0; // Reset it
+            lazy[node] = 0;
         }
 
-        if(a > b || a > j || b < i) // Current segment is not within range [i, j]
+        if (left > right || left > j || right < i)
             return;
 
-        if(a >= i && b <= j) { // Segment is fully within range
+        if (left >= i && right <= j) {
             tree[node] += value;
 
-            if(a != b) { // Not leaf node
-                lazy[node*2] += value;
-                lazy[node*2+1] += value;
+            if (left != right) {
+                lazy[leftChild] += value;
+                lazy[rightChild] += value;
             }
 
             return;
         }
+        int middle = getMiddle(left, right);
 
-        updateTree(node * 2, a, (a + b) / 2, i, j, value); // Updating left child
-        updateTree(1 + node * 2, 1 + (a + b) / 2, b, i, j, value); // Updating right child
+        updateTree(leftChild, left, middle, i, j, value);
+        updateTree(rightChild, middle + 1, right, i, j, value);
 
-        tree[node] = Math.max(tree[node * 2], tree[node * 2 + 1]); // Updating root with max value
+
+        tree[node] = Math.max(tree[leftChild], tree[rightChild]);
     }
+
     /**
      * Returns the maximum value in the interval [idx1, idx2].
      */
     int query(int idx1, int idx2) {
 
-        return query_tree(1,0,n-1,idx1,idx2);
+        return query_tree(1, 0, n - 1, idx1, idx2);
 
     }
-    int query_tree(int node, int a, int b, int i, int j) {
 
-        if(a > b || a > j || b < i) return Integer.MIN_VALUE; // Out of range
+    int query_tree(int node, int left, int right, int i, int j) {
 
-        if(lazy[node] != 0) { // This node needs to be updated
-            tree[node] += lazy[node]; // Update it
+        if (left > right || left > j || right < i) {
+            return Integer.MIN_VALUE;
+        }
+        int leftChild = 2 * node;
+        int rightChild = leftChild + 1;
+        if (lazy[node] != 0) {
+            tree[node] += lazy[node];
 
-            if(a != b) {
-                lazy[node*2] += lazy[node]; // Mark child as lazy
-                lazy[node*2+1] += lazy[node]; // Mark child as lazy
+            if (left != right) {
+                lazy[leftChild] += lazy[node];
+                lazy[rightChild] += lazy[node];
             }
 
-            lazy[node] = 0; // Reset it
+            lazy[node] = 0;
         }
 
-        if(a >= i && b <= j) // Current segment is totally within range [i, j]
+        if (left >= i && right <= j) {
             return tree[node];
+        }
 
-        int q1 = query_tree(node*2, a, (a+b)/2, i, j); // Query left child
-        int q2 = query_tree(1+node*2, 1+(a+b)/2, b, i, j); // Query right child
+        int middle = getMiddle(left, right);
 
-        int res = Math.max(q1, q2); // Return final result
+        if (i > middle) {
+            return query_tree(rightChild, middle + 1, right, i, j);
+        }
+        if (j <= middle) {
+            return query_tree(leftChild, left, middle, i, j);
+        }
 
-        return res;
+        int q1 = query_tree(leftChild, left, middle, i, j);
+        int q2 = query_tree(rightChild, middle + 1, right, i, j);
+
+        int result = Math.max(q1, q2);
+
+        return result;
     }
 
-};
+}
